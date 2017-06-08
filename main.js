8ff1d736ae6f4dde883906ef07557ace9be6230f
Joe Shepherd <joeshepmedia@gmail.com>
2017-06-08 13:29:05 -0500

    Initial commit; Add pics to DOM w/CSS

---
+++ main.js
@@ -0,0 +1,5 @@
+let monkeyBtn = document.getElementById("monkeyfy");
+let horseBtn = document.getElementById("horsefy");
+let dogPics = document.querySelectorAll(".dog-card");
+let rainbowPics = document.querySelectorAll(".rainbow-card")
+

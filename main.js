238b6c187a1ac4ed12d396a55265a9ff7e3bc536
Joe Shepherd <joeshepmedia@gmail.com>
2017-06-08 14:05:24 -0500

    Move duplicate callback code into single function

--- main.js
+++ main.js
@@ -14,29 +14,24 @@ for ( let i = 0; i < rainbowPics.length; i++) {
 }
 
 monkeyBtn.addEventListener("click", function() {
-  monkeyBtn.classList.toggle("dogify");
-  if (monkeyBtn.classList.contains("dogify")) {
-    monkeyBtn.innerHTML = "Dogify it!" 
-    dogH1.innerHTML = "My Awesome Monkey Pics"
-  } else {
-    monkeyBtn.innerHTML = "Monkeyfy it!"
-    dogH1.innerHTML = "My Awesome Dog Pics"
-  }
-  for( let i = 0; i < dogPics.length; i++) {
-    dogPics[i].classList.toggle("fancy-border");
-  }
+  updateCard(dogPics, dogH1, monkeyBtn, "Dog", "Monkey")
 })
 
 horseBtn.addEventListener("click", function() {
-  horseBtn.classList.toggle("rainbowify");
-  if (horseBtn.classList.contains("dogify")) {
-    horseBtn.innerHTML = "Dogify it!" 
-    rainbowH1.innerHTML = "My Awesome Rainbow Pics"
-  } else {
-    horseBtn.innerHTML = "Rainbowify it!"
-    rainbowH1.innerHTML = "My Awesome Horse Pics"
-  }
-  for( let i = 0; i < rainbowPics.length; i++) {
-    rainbowPics[i].classList.toggle("fancy-border");
-  }
+  updateCard(rainbowPics, rainbowH1, horseBtn, "Rainbow", "Horse")
 })
+
+function updateCard(pics, h1, btn, cat1, cat2) {
+  console.log("updateCard called");
+  btn.classList.toggle(`${cat1}ify`);
+  if (btn.classList.contains(`${cat1}ify`) ) {
+    btn.innerHTML = `${cat1}ify it!`;
+    h1.innerHTML = `My Awesome ${cat2} Pics`
+  } else {
+    btn.innerHTML = `${cat2}ify it!`;
+    h1.innerHTML = `My Awesome ${cat1} Pics`
+  }
+  for ( let i = 0; i < pics.length; i++) {
+    pics[i].classList.toggle("fancy-border")
+  } 
+}
